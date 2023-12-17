3482bca7f30a9395154ba5b0f02baaa4a937477a
MendezCarlos-FS <freakinboyo@student.fullsail.edu>
2023-12-16 20:03:32 -0400

    Added to bypass CORS

--- app/index.js
+++ app/index.js
@@ -3,6 +3,14 @@ const morgan = require("morgan");
 const app = express();
 const routeHandler = require("./routes");
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use(express.json());
 app.use(morgan("dev"));
 
